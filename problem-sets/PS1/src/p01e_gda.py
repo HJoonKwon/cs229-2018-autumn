2594ceeff9d2a9e76cd63124a02ff3531beab848
HJoonKwon <dsazz801@gmail.com>
2023-01-21 21:14:02 +0900

    Implemnt GDA

--- problem-sets/PS1/src/p01e_gda.py
+++ problem-sets/PS1/src/p01e_gda.py
@@ -1,7 +1,7 @@
 import numpy as np
-import util
 
-from linear_model import LinearModel
+from src import util
+from src.linear_model import LinearModel
 
 
 def main(train_path, eval_path, pred_path):
@@ -14,8 +14,15 @@ def main(train_path, eval_path, pred_path):
     """
     # Load dataset
     x_train, y_train = util.load_dataset(train_path, add_intercept=False)
+    x_test, y_test = util.load_dataset(eval_path, add_intercept=True)
 
     # *** START CODE HERE ***
+    model = GDA()
+    model.fit(x_train, y_train)
+    preds = model.predict(x_test)
+    preds = preds >= 0.5
+    acc = np.sum(preds == y_test) / y_test.shape[0]
+    print(f"Accuracy on testset is {acc}")
     # *** END CODE HERE ***
 
 
@@ -42,18 +49,27 @@ class GDA(LinearModel):
         m, n = x.shape
         y = y.reshape((-1, 1))
 
-        self.theta = np.zeros((n+1, 1))
+        self.theta = np.zeros((n + 1, 1))
 
-        phi = 1 / m * np.sum(y==1)
-        mu0 = np.sum((y==0) * x, axis=0) / np.sum(y==0)
-        mu1 = np.sum((y==1) * x) / np.sum(y==1)
-        cov = 1/m *  (x - (y==0)*mu0 - (y==1)*mu1).T @ (x - (y==0)*mu0 - (y==1)*mu1)
+        phi = 1 / m * np.sum(y == 1)
+        mu0 = np.sum((y == 0) * x, axis=0) / np.sum(y == 0)
+        mu1 = np.sum((y == 1) * x, axis=0) / np.sum(y == 1)
 
-        theta = np.linalg.inv(cov) @ (mu1 - mu0).reshape(-1, 1)
-        theta0 = 0.5 * (mu0 + mu1).T @ np.linalg.inv(cov) @ (mu0 - mu1) - np.log(1-phi/phi)
+        cov = 1 / m * (x - (y == 0) * mu0 -
+                       (y == 1) * mu1).T @ (x - (y == 0) * mu0 -
+                                            (y == 1) * mu1)
+
+        mu0 = mu0.reshape(-1, 1)
+        mu1 = mu1.reshape(-1, 1)
+        theta = np.linalg.inv(cov) @ (mu1 - mu0)
+
+        theta0 = 0.5 * (mu0 +
+                        mu1).T @ np.linalg.inv(cov) @ (mu0 - mu1) - np.log(
+                            (1 - phi) / phi)
+        theta0 = np.squeeze(theta0)
+
         self.theta[0] = theta0
-
-
+        self.theta[1:] = theta
 
         # *** END CODE HERE ***
 
@@ -67,4 +83,7 @@ class GDA(LinearModel):
             Outputs of shape (m,).
         """
         # *** START CODE HERE ***
+        z = x @ self.theta
+        htheta = 1 / (1 + np.exp(-z))
+        return htheta.squeeze()
         # *** END CODE HERE
